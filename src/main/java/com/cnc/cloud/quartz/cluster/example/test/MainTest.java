9188edab77e6ca23303153d172e20581ae16b0ce
MusicXi <792331407@qq.com>
2017-12-20 18:29:06 +0800

    ddd

--- src/main/java/com/cnc/cloud/quartz/cluster/example/test/MainTest.java
+++ src/main/java/com/cnc/cloud/quartz/cluster/example/test/MainTest.java
@@ -1,4 +1,4 @@
-package com.sundoctor.quartz.cluster.example.test;  
+package com.cnc.cloud.quartz.cluster.example.test;  
   
 import org.springframework.context.ApplicationContext;  
 import org.springframework.context.support.ClassPathXmlApplicationContext;  
@@ -9,7 +9,7 @@ public class MainTest {
      * @param args 
      */  
     public static void main(String[] args) {  
-        ApplicationContext springContext = new ClassPathXmlApplicationContext(new String[]{"classpath:spring-context.xml","classpath:spring-context-quartz.xml"});  
+        ApplicationContext springContext = new ClassPathXmlApplicationContext(new String[]{"classpath:spring/spring-context.xml","classpath:spring/spring-context-quartz.xml"});  
     }  
   
 }  
